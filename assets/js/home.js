70a2aae7f2a2130632f669592bc2be5d98cab8e9
emilaa <emilaa@code.edu.az>
2022-09-11 11:32:11 +0400

    In Main, I created 'intro-slider', 'site-info', 'product-card', 'product-card-2' section

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,18 @@
 let swiper = new Swiper(".mySwiper", {
   loop: true,
 });
+
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 4,
+//   spaceBetween: 30,
+//   loop: true,
+//   loopFillGroupWithBlank: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
