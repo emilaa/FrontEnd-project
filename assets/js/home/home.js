3c8ec8f67eff029db320d1887157b4c23b26e613
emilaa <emilaa@code.edu.az>
2022-09-12 21:05:46 +0400

    I created Accessories page.

--- assets/js/home/home.js
+++ assets/js/home/home.js
@@ -16,5 +16,17 @@ $(function() {
       $(this).addClass('filled');
     }
   })
-})
 
+  let scrollSection = document.getElementById("scroll-section");
+   
+   window.onscroll = function() {scrollFunction()};
+
+   function scrollFunction() {
+     if (document.body.scrollTop > 195|| document.documentElement.scrollTop > 195) {
+        scrollSection.style.top = "0";
+     } else {
+        scrollSection.style.top = "-62px";
+        $("div").removeClass("inActive");
+     }
+   }
+})
