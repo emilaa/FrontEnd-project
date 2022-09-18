b3336e6199347ab64a57ff36066c2894944dc8c8
emilaa <emilaa@code.edu.az>
2022-09-19 01:46:07 +0400

    I created Cart page and responsive Home page.

--- assets/js/cart/cart.js
+++ assets/js/cart/cart.js
@@ -1,3 +1,4 @@
+"use strict";
 $(function () {
   let scrollSection = document.getElementById("scroll-section");
 
@@ -17,3 +18,44 @@ $(function () {
     }
   }
 });
+
+let products = [];
+
+if (localStorage.getItem("products") != null) {
+  products = JSON.parse(localStorage.getItem("products"));
+}
+
+let tableBody = document.querySelector(".table .table-body");
+
+for (const product of products) {
+  tableBody.innerHTML += `<tr>
+    <td product-id="${product.id}"><img src="${product.image}" height="100px" class="card-img-top" alt="..."></td>
+    <td>${product.name}</td>
+    <td>${product.cost}</td>
+    <td>${product.count}</td>
+    <td class="icon-del"><i class="fa-solid fa-trash"></i></td>
+  </tr>`;
+}
+let deleteIcon = document.querySelectorAll(".icon-del i");
+
+deleteIcon.forEach((element) => {
+  element.addEventListener("click", function () {
+    let id = parseInt(
+      this.parentNode.parentNode.firstElementChild.getAttribute("product-id")
+    );
+
+    products = products.filter((m) => m.id != id);
+    localStorage.setItem("products", JSON.stringify(products));
+    this.parentNode.parentNode.remove();
+    document.querySelector("sup").innerText = products.length;
+
+    Swal.fire({
+      position: "top-center",
+      icon: "success",
+      title: "Product deleted",
+      showConfirmButton: false,
+      timer: 1000,
+    });
+  });
+});
+
