c22f75a9e8005156e75878776d19bbbe4c92ab1a
emilaa <emilaa@code.edu.az>
2022-09-18 13:32:25 +0400

    I created responsive Accessories page.

--- assets/js/accessories/accessories.js
+++ assets/js/accessories/accessories.js
@@ -1,14 +1,33 @@
-$(function(){
-    let scrollSection = document.getElementById("scroll-section");
-   
-    window.onscroll = function() {scrollFunction()};
- 
-    function scrollFunction() {
-      if (document.body.scrollTop > 195|| document.documentElement.scrollTop > 195) {
-         scrollSection.style.top = "0";
-      } else {
-         scrollSection.style.top = "-62px";
-         $("div").removeClass("inActive");
-      }
+"use strict";
+$(function () {
+  let scrollSection = document.getElementById("scroll-section");
+
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 195 ||
+      document.documentElement.scrollTop > 195
+    ) {
+      scrollSection.style.top = "0";
+    } else {
+      scrollSection.style.top = "-200px";
+      $("div").removeClass("inActive");
     }
-})
+  }
+
+  let swiper = new Swiper(".mySwiper", {
+    loop: true,
+  });
+
+  let dropdownBtnn = document.querySelectorAll(".dropdown-btnn");
+
+  dropdownBtnn.forEach((element) => {
+    element.addEventListener("click", function () {
+      console.log(this.nextElementSibling);
+      this.nextElementSibling.classList.toggle("drop-show");
+    });
+  });
+});
